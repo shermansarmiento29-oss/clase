e79875a39fd7d282abb707f6f96d387d673a64c5
sherman sarmiento <shermansarmiento29@gmail.com>
2026-07-31 12:06:27 -0500

    Add files via upload

---
+++ setInterval/setInterval.js
@@ -0,0 +1,23 @@
+setInterval(() => {
+            document.getElementById("texto").innerText = "¡Adiós!";
+        }, 2000);
+
+
+
+
+ // Guardamos el elemento de la pantalla en una variable
+        // const elementoTexto = document.getElementById("texto");
+
+        // // El temporizador se ejecuta cada 2 segundos
+        // setInterval(() => {
+            
+        //     // CONDICIÓN: Si el texto actual es "Hola"
+        //     if (elementoTexto.innerText === "Hola") {
+        //         elementoTexto.innerText = "¡Adiós!"; // Cambia al otro texto
+        //     } 
+        //     // Si el texto NO es "Hola" (es decir, ya es "¡Adiós!")
+        //     else {
+        //         elementoTexto.innerText = "Hola"; // Regresa al texto original
+        //     }
+
+        // }, 2000);
